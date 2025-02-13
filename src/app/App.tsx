4c5b8a6f344a487f07601b2d01db39f8754ad6b6
AlekseySimonov <aleksey.simonov04@mail.ru>
2025-02-13 12:46:15 +0300

    Installed RadixUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import './app.scss'
+import "@radix-ui/themes/styles.css";
 
 export const App = () => {
 	return (
 		<div className='text'>
 			<div className='text-header'>Configured Geant4 app</div>
 		</div>
-			
 	)
 }
