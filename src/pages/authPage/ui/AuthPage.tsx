ffb8d25755d86bb2cd9301ca6d7bb991f0338771
AlekseySimonov <aleksey.simonov04@mail.ru>
2025-03-29 15:04:44 +0700

    checkout

--- src/pages/authPage/ui/AuthPage.tsx
+++ src/pages/authPage/ui/AuthPage.tsx
@@ -4,15 +4,15 @@ import backgroundImg from '@/shared/ui/assets/authBackground.jpg'
 
 const AuthPage: React.FC = () => {
 	return (
-		<div className= {styles.container}>
+		<div className={styles.container}>
 			<img className={styles.container__background} src={backgroundImg} alt="" />
 			<div className={styles.container__content}>
 				<div className={styles.container__content_header}>
 					<span>Geant4</span>
 					Добро пожаловать
-				</div> 
-				<Outlet/>
-            </div>
+				</div>
+				<Outlet />
+			</div>
 		</div>
 	)
 }
