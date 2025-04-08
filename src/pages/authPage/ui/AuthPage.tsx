9e4510d059687f8cb514bca478d5787223f37af6
AlekseySimonov <aleksey.simonov04@mail.ru>
2025-04-09 00:44:05 +0700

    Fixed adaptive

--- src/pages/authPage/ui/AuthPage.tsx
+++ src/pages/authPage/ui/AuthPage.tsx
@@ -1,17 +1,14 @@
 import { Outlet } from 'react-router'
 import styles from './_authpage.module.scss'
-import backgroundImg from '@/shared/ui/assets/authBackground.jpg'
 import { Loader } from '@/shared'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/app/store'
-import { Container } from '@mui/material'
 
 const AuthPage: React.FC = () => {
 
 	const { isLoading} = useSelector((state: RootState) => state.mutationStatus)
 
 	return (
-		<Container maxWidth ='md'>
 			<div className={styles.container}>
 			<div className={styles.container__content}>
 				<div className={styles.container__content_header}>
@@ -22,8 +19,6 @@ const AuthPage: React.FC = () => {
 				{isLoading && <Loader /> }
 			</div>
 		</div>
-		</Container>
-		
 		
 	)
 }
